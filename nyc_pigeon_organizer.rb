5c0c7a2e07eda27b2d97008b1e4692bb72e584c0
Michael Calkins <m.calkins576@gmail.com>
2019-10-24 17:46:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,35 @@
+require 'pry'
+
+
 def nyc_pigeon_organizer(data)
   # write your code here!
-end
+  new_sort_1 = {}
+  #binding.pry
+  data.each_value do |n|
+   # binding.pry
+    n.each_value do |k|
+     # binding.pry
+      k.map {|b| new_sort_1[b]=data unless new_sort_1.has_key?(b)}
+      
+    end
+  end
+  new_sort_1
+  new_sort_2 = Marshal.load(Marshal.dump(new_sort_1))
+    new_sort_1.each do |ka,va|
+      va.each do |kb,vb|
+        vb.each do |kc,vc|
+          #binding.pry
+          if vc.include?("#{ka}") && new_sort_2[ka][kb].is_a?(Array)
+            new_sort_2["#{ka}"][kb] << "#{kc}"
+          elsif vc.include?("#{ka}")
+            new_sort_2["#{ka}"][kb] = Array.new
+            new_sort_2["#{ka}"][kb] << "#{kc}"
+          else
+           binding.pry
+          end
+        end 
+      end
+    end
+    binding.pry
+    new_sort_2
+  end
