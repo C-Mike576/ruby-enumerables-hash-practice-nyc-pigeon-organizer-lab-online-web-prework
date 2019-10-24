8cc0c643fd1cf027d1d9b5eb2e4ac056ccb6c69f
Michael Calkins <m.calkins576@gmail.com>
2019-10-24 18:44:49 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+cage = Hash.new do |h, k|
+  h[k] = Hash.new do |ih, ik|
+    ih[ik] = []
+  end
+end
+
+data.each do |atrabu, set|
+  set.each do |value, names|
+    names.each do |name|
+      cage[name][atrabu] << value.to_s
+    end
+  end
+end
+ cage
 end
