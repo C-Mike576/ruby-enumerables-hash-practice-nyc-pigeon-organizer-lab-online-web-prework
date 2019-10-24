1b82897a8d977820a9d16fadf80a3a71a76d3bc4
Michael Calkins <m.calkins576@gmail.com>
2019-10-24 17:58:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,17 +4,25 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
 cage = {}
+
+
+def find_color(data)
 data[:color].each_with_object(Hash.new {|h,k| h[k] = {color:[]}}) do |(color,birds),cage| 
   birds.each do |bird| 
     cage[bird][:color] << color.to_s 
+  end
+end
+
+      
+      
       data[:gender].each_with_object(Hash.new {|h,k| h[k] = {gender:[]}}) do |(gender,birds),cage| 
         birds.each do |bird| 
           cage[bird][:gender] << gender.to_s 
+            
+            
             data[:lives].each_with_object(Hash.new {|h,k| h[k] = {lives:[]}}) do |(lives,birds),cage| 
               birds.each do |bird| 
                 cage[bird][:lives] << lives.to_s
-              end
-          end
 
 cage
 
